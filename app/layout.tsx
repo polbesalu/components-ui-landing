5f5630b6f3d26f72ab43d1dbc0d8cbadeb56f5aa
polbesalu <87153415+polbesalu@users.noreply.github.com>
2025-02-05 18:22:44 +0100

    components

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import type React from "react" // Added import for React
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "shadcn/ui",
+  title: "components/ui",
   description: "Beautifully designed components built with Radix UI and Tailwind CSS.",
 }
 
